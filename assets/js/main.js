c78d819b78910a36fb8ad14a99b57f331fed4086
zogorn <king.zogorn@gmail.com>
2017-03-14 07:19:50 -0700

    added js function to set the height of bigIMage section and upload some photos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+var wHeight = window.innerHeight,
+	navHeight = document.querySelector('.navbar').offsetHeight;
+
+function setHeight(element) {
+	element.style.height = (wHeight - navHeight) + 'px' ;
+}
+
 document.ready = (function(){
-	
+	setHeight(bigImage);
 }());
