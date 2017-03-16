72d2da8144440bc0b8d583bd8dda390d3d78d23c
zogorn <king.zogorn@gmail.com>
2017-03-16 22:22:24 +0200

    delete books and rerecognize folers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-var wHeight = window.innerHeight,
-	navHeight = document.querySelector('.navbar').offsetHeight;
+var wHeight = window.innerHeight, // get window height
+	navHeight = document.querySelector('.navbar').offsetHeight; // get the navigationbar height
 
+
+// set the height to full screen
 function setHeight(element) {
 	element.style.height = (wHeight - navHeight) + 'px' ;
-}
+} // End setHeight
 
 document.ready = (function(){
 	setHeight(bigImage);
